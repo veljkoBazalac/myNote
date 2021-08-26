c5f32d2876303bc2c0a3a068b841d0e3a56210c2
Veljko <veljkobazalac@gmail.com>
2021-08-26 13:28:20 +0200

    Initial Commit

---
+++ myNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myNote
+//
+//  Created by Veljko on 26.8.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
