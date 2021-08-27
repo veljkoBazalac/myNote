11164eaafde236aa7bf1506974c964e5b7ad8e4e
Veljko <veljkobazalac@gmail.com>
2021-08-27 11:55:02 +0200

    Pods and Workspace added

---
+++ Podfile
@@ -0,0 +1,10 @@
+
+platform :ios, '13.0'
+
+target 'myNote' do
+
+  # Pods for myNote
+  
+#  pod 'KeyboardHideManager', '~> 1.0'
+
+end
---
+++ Pods/Target Support Files/Pods-myNote/Pods-myNote-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_myNote : NSObject
+@end
+@implementation PodsDummy_Pods_myNote
+@end
